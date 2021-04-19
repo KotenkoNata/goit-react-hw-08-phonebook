afa5b6067ef9f847899459613b55c7af66340334
KotenkoNata <kotenkonata8@gmail.com>
2021-04-19 21:56:05 +0300

    Updates

---
+++ static/js/6.ef15f33a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-pg"]=this["webpackJsonpgoit-react-pg"]||[]).push([[6],{117:function(t,e,s){"use strict";s.r(e);s(0);var c=s(1);e.default=function(){return Object(c.jsx)("div",{className:"HomeView"})}}}]);
+//# sourceMappingURL=6.ef15f33a.chunk.js.map
